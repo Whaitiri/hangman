d8b6153df679eba75f856131f6a632429525e0a6
Tuterangi Pleydell <tuterangi@gmail.com>
2018-03-02 19:36:53 +1300

    prints summary at end, fixed end of game check

--- hungmen.rb
+++ hungmen.rb
@@ -1,13 +1,14 @@
 #!/usr/bin/env ruby
-$playerGuessNum = 10
+$playerGuessNum = 1
 
 class Player
-  attr_accessor :playerGuess, :playerName, :playerGuesses
-  def createPlayer
-    puts "What is your name"
+  attr_accessor :playerGuess, :playerName, :playerGuesses, :playerNum
+  def createPlayer(playerNum)
+    puts "What is your name, Player #{playerNum}?"
     @playerName = gets.chomp
     @playerGuess = $playerGuessNum
     @playerGuesses = []
+    @playerNum = playerNum
   end
 
   def loseGuess
@@ -29,7 +30,7 @@ class Game
     if response == 1 or response == 2
       response.times do |x|
         @players[x] = Player.new
-        @players[x].createPlayer
+        @players[x].createPlayer(x+1)
       end
     else
       initialize
@@ -53,9 +54,9 @@ class Game
 
   def input
     @players.each do |player|
-      if player.playerGuess == 0
-        puts "You have no coins left!"
-        exit
+      if player.playerGuess < 1
+        puts "#{player.playerName} has run out of coins!"
+        next
       end
       puts "Press [y] to generate a word, #{player.playerName}; you have #{player.playerGuess} coins remaining."
       if player.playerGuesses.length > 0
@@ -71,11 +72,41 @@ class Game
         puts word
         player.storeGuess(word)
         player.loseGuess
+      elsif response == 'n'
+        next
       else
         exit
       end
     end
-    self.input
+    if self.checkGuess
+      exit
+    else
+      self.input
+    end
+  end
+
+  def checkGuess
+    noCoins = 0
+    @players.each do |player|
+      if player.playerGuess < 1
+        noCoins += 1
+      end
+    end
+    if noCoins >= @players.length
+      puts "All players have run out of coins!"
+      puts "Summary:"
+      @players.each do |player|
+        puts "#{player.playerName}'s words:"
+        player.playerGuesses.each do |guess|
+          print "#{guess} "
+        end
+        puts ""
+        puts ""
+      end
+      return true
+    else
+      return false
+    end
   end
 
 end
