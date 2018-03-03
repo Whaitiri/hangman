fdfe284eff9fc6a6443993b5af978ea8af970b3b
Tuterangi Pleydell <tuterangi@gmail.com>
2018-03-03 17:29:04 +1300

    almost finished hangman, however players share playerWord variable

--- hungmen.rb
+++ hungmen.rb
@@ -1,36 +1,40 @@
 #!/usr/bin/env ruby
-$playerGuessNum = 1
+$playerGuessNum = 10
 
 class Player
-  attr_accessor :playerGuess, :playerName, :playerGuesses, :playerNum
+  attr_accessor :playerGuess, :playerWord, :playerName, :playerGuesses, :playerNum
   def createPlayer(playerNum)
     puts "What is your name, Player #{playerNum}?"
     @playerName = gets.chomp
     @playerGuess = $playerGuessNum
     @playerGuesses = []
+    @playerWord = []
     @playerNum = playerNum
   end
 
-  def loseGuess
-    @playerGuess -= 1
+  def addGuess(i,guess)
+    @playerWord[i] = guess
   end
-
-  def storeGuess(x)
-    @playerGuesses << x
-  end
-
-
 end
 
 class Game
   def initialize
     @players = []
+    @currentWord = self.callWord
+    @currentWordBlanked = []
+    @currentWord.each do
+      @currentWordBlanked << '_'
+    end
+
     puts "How many players? 1 or 2"
     response = Integer(gets.chomp)
     if response == 1 or response == 2
       response.times do |x|
         @players[x] = Player.new
         @players[x].createPlayer(x+1)
+      end
+      @players.each do |player|
+        player.playerWord = @currentWordBlanked
       end
     else
       initialize
@@ -49,43 +53,92 @@ class Game
         wordList << line.strip
       end
     end
-    return wordList[rand(0...wordList.count)]
+    return wordList[rand(0...wordList.count)].split("")
   end
 
   def input
     @players.each do |player|
       if player.playerGuess < 1
-        puts "#{player.playerName} has run out of coins!"
+        puts "#{player.playerName} has run out of guesses!"
         next
       end
-      puts "Press [y] to generate a word, #{player.playerName}; you have #{player.playerGuess} coins remaining."
-      if player.playerGuesses.length > 0
-        puts "#{player.playerName}'s previous words:"
-        player.playerGuesses.each do |guess|
-          print "#{guess}, "
-        end
-        puts ""
-      end
-      response = gets.chomp
-      if response == 'y'
-        word = self.callWord
-        puts word
-        player.storeGuess(word)
-        player.loseGuess
-      elsif response == 'n'
-        next
-      else
+      self.checkGuess(player)
+      if player.playerWord == @currentWord
+        puts "#{player.playerName} won!"
+        self.summarizeGame
         exit
       end
     end
-    if self.checkGuess
+    if self.checkGuessesLeft
       exit
     else
       self.input
     end
   end
 
-  def checkGuess
+  def guessPrompt(player)
+    puts "#{player.playerName}'s turn to guess'; you have #{player.playerGuess} guesses remaining."
+    if player.playerGuesses.length > 0
+      puts "#{player.playerName}'s previous guesses:"
+      player.playerGuesses.each do |guess|
+        print "#{guess} "
+      end
+      puts ""
+    end
+    puts "#{player.playerWord.join(' ')}"
+    print "Input a letter: "
+  end
+
+  def checkGuess(player)
+    begin
+      self.guessPrompt(player)
+      response = gets.chomp.downcase
+      if response == "cheat" #enter cheat for testing
+        puts "TESTING ONLY - THE ANSWER IS #{@currentWord.join}"
+        response = nil
+
+      elsif response == 'exit'
+        exit
+
+      elsif response.length > 1 or response =~ /[^a-z]/
+        puts "please enter a single letter"
+        response = nil
+
+      elsif player.playerGuesses.include?(response)
+        puts "you have already input that letter"
+        response = nil
+
+      end
+      raise
+    rescue
+      retry if response == nil or response == ''
+    end
+    i = 0
+    guessFail = 0
+    @currentWord.each do |letter|
+      if letter == response
+        player.playerWord[i] = letter
+      else
+        guessFail += 1
+      end
+      i += 1
+    end
+
+    if player.playerGuesses.include?(response)
+    else
+      player.playerGuesses << response
+    end
+
+    if guessFail >= i
+      player.playerGuess -= 1
+      puts "#{player.playerName}'s guess was incorrect..."
+    else
+      puts "#{player.playerName}'s guess was correct!"
+    end
+    puts "#{player.playerName}: #{player.playerWord.join(' ')}"
+  end
+
+  def checkGuessesLeft
     noCoins = 0
     @players.each do |player|
       if player.playerGuess < 1
@@ -94,18 +147,23 @@ class Game
     end
     if noCoins >= @players.length
       puts "All players have run out of coins!"
-      puts "Summary:"
-      @players.each do |player|
-        puts "#{player.playerName}'s words:"
-        player.playerGuesses.each do |guess|
-          print "#{guess} "
-        end
-        puts ""
-        puts ""
-      end
+      self.summarizeGame
       return true
     else
       return false
+    end
+  end
+
+  def summarizeGame
+    puts "Summary:"
+    puts "The word was #{@currentWord.join}"
+    @players.each do |player|
+      puts "#{player.playerName}'s guesses:"
+      player.playerGuesses.each do |guess|
+        print "#{guess} "
+      end
+      puts ""
+      puts "#{player.playerName}'s guess was #{player.playerWord.join} with #{player.playerGuess} guesses remaining"
     end
   end
 
